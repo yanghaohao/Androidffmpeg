c37200a11883f306dffd296cba20116332d0bbc5
yanghaohao <871626535@qq.com>
2019-01-14 16:26:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yanghao/speakdemo/MyRelative.java
@@ -0,0 +1,25 @@
+package com.example.yanghao.speakdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class MyRelative extends RelativeLayout {
+
+
+    public MyRelative(Context context) {
+        super(context);
+
+
+    }
+
+    public MyRelative(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRelative(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
